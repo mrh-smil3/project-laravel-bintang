b2fdcf0714571205e26e999551cc94e8f4bef69a
ARIF <50254637+mrh-smil3@users.noreply.github.com>
2023-04-08 23:38:51 +0700

    Add files via upload

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@extends('layouts.navbar')
+@section('content')
+    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Nama</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan data">
+</div>
+<div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">NIM</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan data">
+  </div>
+  <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Kelas</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan data">
+  </div>
+
+@endsection
---
+++ resources/views/frontend/information.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@extends('layouts.navbar')
+@section('content')
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">No</th>
+        <th scope="col">Nama</th>
+        <th scope="col">NIM</th>
+        <th scope="col">Kelas</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Marquwz</td>
+        <td>211240001142</td>
+        <td>E</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Bintang</td>
+        <td>211240001142</td>
+        <td>E</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Kampret</td>
+        <td>211240001131</td>
+        <td>E</td>
+      </tr>
+    </tbody>
+  </table>
+@endsection
